6dcaefa8718427897dd9160461734d4a07004bda
RUSSELL DELGADO MUÑOZ <russellwallstreet@gmail.com>
2021-03-09 17:17:42 +0100

    terminado expliter muñeco moviendose

---
+++ app/src/main/java/com/example/sprite/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sprite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GameView game = new GameView(this);
+        setContentView(game);
+    }
+}
